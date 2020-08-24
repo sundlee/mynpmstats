1b11af7c0ab7d7282e97fa53501221bf56ef2a02
Yong-Hae Lee <sundlee@gmail.com>
2020-08-24 12:42:00 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+	<template>
+  <div id="app">
+    <my-header></my-header>
+    <router-view :key="$route.path"></router-view>
+    <my-footer></my-footer>
+  </div>
+</template>
+
+<script>
+import MyHeader from '@/components/Header'
+import MyFooter from '@/components/Footer'
+export default {
+  components: { MyHeader, MyFooter },
+  name: 'app',
+  metaInfo: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'theme-color', content: '#ffffff' }
+    ]
+  }
+}
+</script>
+
+<style lang="scss">
+//   @import "/assets/styles/_variables.scss";
+//   .Cookie__button {
+//     background: color(robin-egg-blue)!important;
+//     @include hover-active-states() {
+//       background: darken(color(robin-egg-blue), 10%)!important;
+//     }
+//   }
+</style>
+
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import StartPage from '@/pages/Start'
+// import Meta from 'vue-meta'
+
+// Vue.use(Meta)
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Start',
+      component: StartPage
+    },
+    {
+      path: '/:package',
+      name: 'Package',
+      component: StartPage
+    }
+  ]
+});
